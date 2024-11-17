9dd633217413bb2377dec6bea4951bf533942270
Viktor Tymoshenko <tymoshenko1viktor@gmail.com>
2024-11-17 11:57:06 +0000

    updated mod calc

--- Menu.java
+++ Menu.java
@@ -1,35 +1,38 @@
-// for github lab
 import java.util.Scanner;
-public class Menu{
-public static void main(String[] args){
-Scanner input = new Scanner(System.in);
-System.out.println("Select an option (1-4):");
-System.out.println();
-System.out.println("1. If..else");
-System.out.println("2. While");
-System.out.println("3. For");
-System.out.println("4. Switch");
-System.out.println();
-System.out.print("Choice: ");
-int choice = input.nextInt();
-System.out.println();
-switch(choice){
-case 1:
-System.out.println("If..else lab");
-break;
-case 2:
-    System.out.println("While lab");
-break;
-case 3:
-System.out.println("For lab");
-break;
-case 4:
-System.out.println("Switch lab");
-break;
-default:
-System.out.println("Invalid choice");
-break;
-}
-System.out.println();
-}
+
+
+public class Menu
+{
+	public static void main(String[] args)
+	{
+		String run;
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("""
+                ==============================
+                           Mod Finder
+                ==============================
+                """);
+		do
+		{
+			System.out.println("Enter an integers(a mod b): ");
+			a = Integer.parseInt(sc.next());
+			b = Integer.parseInt(sc.next());
+			if (a > 0)
+			{
+				System.out.println(a % b);
+			}else
+			{
+				while (a < 0)
+				{
+					a += b;
+				}
+				System.out.println(a % b);
+			}
+			System.out. println("would you like to run again? (y/n)");
+			run = sc.nextLine();
+			sc.nextLine();
+		}while(run.equalsIgnoreCase("Y"));
+
+	}
 }
